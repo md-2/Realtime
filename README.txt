325fb54e9ac86a33c63f16586c0efea5939c2742
Mark Davies <mark@618designs.co.uk>
2013-04-19 10:56:49 +0100

    initial commit

---
+++ README.txt
@@ -0,0 +1,16 @@
+This module allows realtime colaboration on Drupal nodes.
+
+REQUIREMENTS
+------------
+
+The module uses the Google Realtime APIs client library for PHP. You can find more 
+information on it at the following url: https://developers.google.com/drive/realtime
+
+The module also has dependencies with the following contributed ones:
+- Libraries
+
+INSTALLATION
+------------
+
+1. Download the module from drupal.org, and place it somewhere in the
+'sites/all/modules/' directory.
---
+++ js/realtime.js
@@ -0,0 +1,89 @@
+/**
+ * This function is called the first time that the Realtime model is created
+ * for a file. This function should be used to initialize any values of the
+ * model. In this case, we just create the single string model that will be
+ * used to control our text box. The string has a starting value of 'Hello
+ * Realtime World!', and is named 'text'.
+ * @param model {gapi.drive.realtime.Model} the Realtime root model object.
+ */
+function initializeModel(model) {
+  var string = model.createString(Drupal.settings.realtime.body);
+//  var string = gapi.drive.realtime.model.prototype.createString(Drupal.settings.realtime.body);
+  model.getRoot().set('text', string);
+}
+
+/**
+ * This function is called when the Realtime file has been loaded. It should
+ * be used to initialize any user interface components and event handlers
+ * depending on the Realtime model. In this case, create a text control binder
+ * and bind it to our string model that we created in initializeModel.
+ * @param doc {gapi.drive.realtime.Document} the Realtime document.
+ */
+function onFileLoaded(doc) {
+  var string = doc.getModel().getRoot().get('text');
+
+  // Keeping one box updated with a String binder.
+  var textArea1 = document.getElementById('edit-body-und-0-value');
+  gapi.drive.realtime.databinding.bindString(string, textArea1);
+
+  // Keeping one box updated with a custom EventListener.
+  var textArea2 = document.getElementById('edit-field-body2-und-0-value');
+  var updateTextArea2 = function(e) {
+    textArea2.value = string;
+  };
+  string.addEventListener(gapi.drive.realtime.EventType.TEXT_INSERTED, updateTextArea2);
+  string.addEventListener(gapi.drive.realtime.EventType.TEXT_DELETED, updateTextArea2);
+  textArea2.onkeyup = function() {
+    string.setText(textArea2.value);
+  };
+  updateTextArea2();
+
+  // Enabling UI Elements.
+  textArea1.disabled = false;
+  textArea2.disabled = false;
+  
+}
+
+/**
+ * Options for the Realtime loader.
+ */
+var realtimeOptions = {
+  /**
+   * Client ID from the APIs Console.
+   */
+  clientId: '285812100879.apps.googleusercontent.com',
+
+  /**
+   * The ID of the button to click to authorize. Must be a DOM element ID.
+   */
+  authButtonElementId: 'authorizeButton',
+
+  /**
+   * Function to be called when a Realtime model is first created.
+   */
+  initializeModel: initializeModel,
+
+  /**
+   * Autocreate files right after auth automatically.
+   */
+  autoCreate: true,
+
+  /**
+   * Autocreate files right after auth automatically.
+   */
+   defaultTitle: Drupal.settings.realtime.defaultTitle,
+
+  /**
+   * Function to be called every time a Realtime file is loaded.
+   */
+  onFileLoaded: onFileLoaded,
+}
+
+
+/**
+ * Start the Realtime loader with the options.
+ */
+function startRealtime() {
+  var realtimeLoader = new rtclient.RealtimeLoader(realtimeOptions);
+  realtimeLoader.start();
+}
